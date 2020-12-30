56c4c0aaf0701093b2574c9c4684ebe4d3d6c5fc
Russell <russelldev@protonmail.com>
2020-12-30 09:05:40 +0000

    Add blurryomg funct

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+
+let load = 0;
+let int = setInterval(blurrying, 30);
+
+function blurrying() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+    console.log(load);
+}
